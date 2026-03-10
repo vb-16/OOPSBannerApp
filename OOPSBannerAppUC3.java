de912911bfe9cda2a4ca1f34780f92a0f323468a
Vaishnavi <vaishnavibharat.170807@gmail.com>
2026-03-10 15:45:10 +0530

    UC3: Banner using String.join()

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp UC3 – Banner Application using String.join()
+ *
+ * Refactors UC2 by replacing string concatenation (+)
+ * with String.join() while producing the same banner output.
+ *
+ * @author Vaishnavi
+ * @version 3.0
+ */
+
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "   ***  ", "    ***  ", " ****** ", "    ***** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **     "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", "**     "));
+        System.out.println(String.join(" ", "**     **", "**     **", "****** ", "    ***** "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**     ", "         **"));
+        System.out.println(String.join(" ", "**     **", "**     **", "**     ", "          **"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**     ", "         ** "));
+        System.out.println(String.join(" ", "   ***  ", "    ***  ", " **     ", "    ***** "));
+        System.out.println(String.join(" ", "       ", "       ", "      ", "       "));
+    }
+}
