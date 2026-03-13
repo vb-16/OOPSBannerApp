59366819622f298c7507178ea30bef2bad0a1c95
Vaishnavi <vaishnavibharat.170807@gmail.com>
2026-03-13 22:01:06 +0530

    UC2 Print OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,14 @@
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * Displays the text OOPS in the console.
- *
- * @author Vaishnavi
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        System.out.println("  *****     *****     ******     ***** ");
+        System.out.println(" *     *   *     *    *     *   *     *");
+        System.out.println("*       * *       *   *     *   *      ");
+        System.out.println("*       * *       *   ******     ***** ");
+        System.out.println("*       * *       *   *               *");
+        System.out.println(" *     *   *     *    *         *     *");
+        System.out.println("  *****     *****     *          ***** ");
+
+    }
 }
